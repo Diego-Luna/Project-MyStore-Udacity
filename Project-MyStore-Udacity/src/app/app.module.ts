3e6307415749e181e6a134e98e46d67c9fb25c61
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-03 11:07:35 -0600

    main-home

--- Project-MyStore-Udacity/src/app/app.module.ts
+++ Project-MyStore-Udacity/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainHomeComponent } from './main-home/main-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainHomeComponent
   ],
   imports: [
     BrowserModule,
