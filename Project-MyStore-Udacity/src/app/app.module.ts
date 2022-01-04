c5bf636c8f83f662e97dc23e266f5933844e8ed0
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-04 12:08:41 -0600

    navbar 2/3

--- Project-MyStore-Udacity/src/app/app.module.ts
+++ Project-MyStore-Udacity/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +32,8 @@ import { ProductPageComponent } from './product-page/product-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
