15b1d4f8626d088341943ff90c40a0d022f5dbe5
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-03 19:01:29 -0600

    products-item

---
+++ Project-MyStore-Udacity/src/app/products-item/products-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-products-item',
+  templateUrl: './products-item.component.html',
+  styleUrls: ['./products-item.component.css']
+})
+export class ProductsItemComponent implements OnInit {
+
+  @Input() imgUrl: string = "";
+  @Input() prodcutName: string = "";
+  @Input() productPrice: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
