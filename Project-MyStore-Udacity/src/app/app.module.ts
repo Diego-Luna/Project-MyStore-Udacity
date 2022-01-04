a380e0eee3bd21688c63d483d0fb3fd93d5ba1ee
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-04 05:03:08 -0600

    Routes 1/3

--- Project-MyStore-Udacity/src/app/app.module.ts
+++ Project-MyStore-Udacity/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeSafeSectionComponent } from './home-safe-section/home-safe-section.
 import { ProductsItemComponent } from './products-item/products-item.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HomeComponent } from './home/home.component';
     HomeSafeSectionComponent,
     ProductsItemComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
