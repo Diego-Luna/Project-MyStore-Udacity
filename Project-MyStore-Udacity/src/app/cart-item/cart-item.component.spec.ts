936bdcec3c3d113312573aba806718afe1007fe8
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-06 15:40:47 -0600

    add testhing http

--- Project-MyStore-Udacity/src/app/cart-item/cart-item.component.spec.ts
+++ Project-MyStore-Udacity/src/app/cart-item/cart-item.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ProductCartService } from '../product-cart.service';
+
 import { CartItemComponent } from './cart-item.component';
 
 describe('CartItemComponent', () => {
   let component: CartItemComponent;
   let fixture: ComponentFixture<CartItemComponent>;
 
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [CartItemComponent]
+  //   })
+  //     .compileComponents();
+  // });
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [CartItemComponent]
+    TestBed.configureTestingModule({
+      declarations: [CartItemComponent],
+      // here is my imported Http TestingModule
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: ProductCartService, useClass: FakeApiService }
+      ]
     })
       .compileComponents();
   });
@@ -23,3 +38,30 @@ describe('CartItemComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+class FakeApiService {
+
+  fakeVal = 5;
+  getProducts() {
+    return [{
+      "id": 0,
+      "name": "10 conductive ink kits",
+      "price": 45,
+      "imgUrl": "./assets/ink.png",
+      "productQuantity": 0
+    }, {
+      "id": 0,
+      "name": "10 conductive ink kits",
+      "price": 45,
+      "imgUrl": "./assets/ink.png",
+      "productQuantity": 0
+    }, {
+      "id": 0,
+      "name": "10 conductive ink kits",
+      "price": 45,
+      "imgUrl": "./assets/ink.png",
+      "productQuantity": 0
+    }]
+  }
+
+}
