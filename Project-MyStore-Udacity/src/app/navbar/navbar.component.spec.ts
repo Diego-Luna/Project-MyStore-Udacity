f9748b6fc7d1fe323527c76c3f842722eb816a38
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-06 12:56:35 -0600

    Test Ready

--- Project-MyStore-Udacity/src/app/navbar/navbar.component.spec.ts
+++ Project-MyStore-Udacity/src/app/navbar/navbar.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [ NavbarComponent ]
+  //   })
+  //   .compileComponents();
+  // });
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [NavbarComponent],
+      imports: [RouterTestingModule],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
